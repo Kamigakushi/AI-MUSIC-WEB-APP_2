e238e17d8b9a832895724ae3c51be5aea8ae928d
Kamigakushi <87258030+Kamigakushi@users.noreply.github.com>
2024-02-16 14:00:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+song_1 = "";
+song_2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+function preload ()
+{
+    song = loadSound("Peter pan.mp3");
+    song = loadSound("Harry potter.mp3");
+}
+function setup()
+{
+  canvas = createCanvas(600,500);
+  canvas.position(485,250);
+  
+  video = createCapture(VIDEO);
+  video.hide();
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+  console.log("Model Loaded!");
+}
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX" + leftWristX + "leftWristY" + leftWristY);
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX" + rightWristX + "rightWristY" + rightWristY);
+  }
+}
+function draw()
+{
+  image(video,0,0,600,500);
+}
